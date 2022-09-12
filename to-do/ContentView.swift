05dd75c2949b70c06b4efbe547c929c303f9edcf
karatasebu <karatasebu47@gmail.com>
2022-09-12 09:43:05 +0300

    Initial Commit

---
+++ to-do/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  to-do
+//
+//  Created by Ebubekir Karatas on 12.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ to-do/to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_doApp.swift
+//  to-do
+//
+//  Created by Ebubekir Karatas on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct to_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
